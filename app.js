010f42ac66ebd31e0e1b11351d6bb5e195958096
sonugupta4636 <sonugupta4636@gmail.com>
2017-11-20 02:59:34 +0530

    Initial Commit
     * Did basic setup and installed required packages.
     * Designed MVC architecture for project.

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var todoController = require('./controllers/todoController');
+
+var app = express();
+
+// setting up template engine
+app.set('view engine', 'ejs');
+
+//static files
+app.use(express.static('./public'));
+
+
+// fire controllers
+todoController(app);
+
+// listening on port
+app.listen(3000);
+console.log('You are listening on port 3000');
